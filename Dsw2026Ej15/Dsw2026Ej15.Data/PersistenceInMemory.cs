baec23f50f626262c8babfb408d1f1566ac17f57
LuciaHidalgo <Lucia.HidalgoBevacqua@alu.frt.utn.edu.ar>
2026-06-19 15:37:44 -0300

    Ej Terminado

--- Dsw2026Ej15/Dsw2026Ej15.Data/PersistenceInMemory.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Data/PersistenceInMemory.cs
@@ -1,4 +1,5 @@
-﻿using Dsw2026Ej15.Domain.Entitys;
+﻿using Dsw2026Ej15.Data.DTOs;
+using Dsw2026Ej15.Domain.Entitys;
 using Dsw2026Ej15.Domain.Interfaz;
 using System;
 using System.Collections.Generic;
@@ -8,82 +9,82 @@ using System.Xml.Linq;
 
 namespace Dsw2026Ej15.Data
 {
-   
-        public class PersistenceInMemory : IPersistence
+
+    public class PersistenceInMemory : IPersistence
+    {
+
+
+        private List<Doctor> Doctors = new List<Doctor>();
+
+        private List<Speciality> Specialities = new List<Speciality>();
+
+
+        public PersistenceInMemory()
+
         {
 
+            CargarEspecialidades();
 
-            private readonly List<Doctor> Doctors = new List<Doctor>();
-
-            private readonly List<Speciality> Specialities = new List<Speciality>();
-
-
-            public PersistenceInMemory()
-
-            {
-
-                CargarEspecialidades();
-
-            }
+        }
 
 
-            public List<Doctor> GetDoctors() => Doctors;
+        public List<Doctor> GetDoctors() => Doctors;
 
 
-            public Doctor? GetDoctor(Guid id)
+        public Doctor? GetDoctor(Guid id)
 
-            {
+        {
 
-                return Doctors.Find(d => d._id == id);
+            return Doctors.Find(d => d._id == id);
 
-            }
-
-            
-            public void AñadirDoctor(Doctor doctor) => Doctors.Add(doctor);
+        }
 
 
-            public void ModificarDoctor(Doctor doctor)
+        public void AñadirDoctor(Doctor doctor) => Doctors.Add(doctor);
+
+
+        public void ModificarDoctor(Doctor doctor)
+
+        {
+
+            var DoctorExistente = GetDoctor(doctor._id);
+
+            if (DoctorExistente != null)
 
             {
 
-                var DoctorExistente = GetDoctor(doctor._id);
+                DoctorExistente._name = doctor._name;
 
-                if (DoctorExistente != null)
+                DoctorExistente._licenseNumber = doctor._licenseNumber;
 
-                {
+                DoctorExistente._speciality = doctor._speciality;
 
-                    DoctorExistente._name = doctor._name;
-
-                    DoctorExistente._licenseNumber = doctor._licenseNumber;
-
-                    DoctorExistente._speciality = doctor._speciality;
-
-                    DoctorExistente._isActive = doctor._isActive;
-
-                }
+                DoctorExistente._isActive = doctor._isActive;
 
             }
 
+        }
 
 
-            public void EliminarDoctor(Guid id)
+
+        public void EliminarDoctor(Guid id)
+
+        {
+
+            Doctor? DoctorEliminar = GetDoctor(id);
+
+            if (DoctorEliminar != null)
 
             {
 
-                Doctor? DoctorEliminar = GetDoctor(id);
-
-                if (DoctorEliminar != null)
-
-                {
-
-                    Doctors.Remove(DoctorEliminar);
-
-                }
+                Doctors.Remove(DoctorEliminar);
 
             }
 
+        }
 
-            public void DesactivarDoctor(Guid id)
+
+        public void DesactivarDoctor(Guid id)
         {
             Doctor? doctorToDeactivate = GetDoctor(id);
             if (doctorToDeactivate != null && doctorToDeactivate._isActive)
@@ -96,72 +97,69 @@ namespace Dsw2026Ej15.Data
         public List<Speciality> GetSpecialities() => Specialities;
 
 
-            public Speciality? GetSpeciality(Guid id)
+        public Speciality? GetSpeciality(Guid id)
+
+        {
+
+            return Specialities.Find(d => d._id == id);
+
+        }
+
+
+        public void AñadirEspecialidad(Speciality speciality) => Specialities.Add(speciality);
+
+
+        public void ModificarEspecialidad(Speciality speciality)
+
+        {
+
+            var EspecialidadExistente = GetSpeciality(speciality._id);
+
+            if (EspecialidadExistente != null)
 
             {
 
-                return Specialities.Find(d => d._id == id);
+                EspecialidadExistente._description = speciality._description;
+
 
             }
 
+        }
 
-            public void AñadirEspecialidad(Speciality speciality) => Specialities.Add(speciality);
 
 
-            public void ModificarEspecialidad(Speciality speciality)
+        public void EliminarEspecialidad(Guid id)
+
+        {
+
+            Doctor? EspecialidadEliminar = GetDoctor(id);
+
+            if (EspecialidadEliminar != null)
 
             {
 
-                var EspecialidadExistente = GetSpeciality(speciality._id);
-
-                if (EspecialidadExistente != null)
-
-                {
-
-                    EspecialidadExistente._description = speciality._description;
-
-
-                }
+                Doctors.Remove(EspecialidadEliminar);
 
             }
 
+        }
 
 
-            public void EliminarEspecialidad(Guid id)
-
+        private void CargarEspecialidades()
+        {
+            try
             {
-
-                Doctor? EspecialidadEliminar = GetDoctor(id);
-
-                if (EspecialidadEliminar != null)
-
-                {
-
-                    Doctors.Remove(EspecialidadEliminar);
-
-                }
+                string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Source", "specialities.json");
+                var json = File.ReadAllText(jsonPath);
+                var specialities = JsonSerializer.Deserialize<List<SpecialityDto>>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true }) ?? [];
+                Specialities = specialities.Select(s => new Speciality(s._name, s._description, s._id)).ToList();
 
             }
-
-
-            private void CargarEspecialidades()
-
+            catch (Exception)
             {
-
-                var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "specialities.json");
-
-                if (File.Exists(filePath))
-                {
-                    var json = File.ReadAllText(filePath);
-
-                    var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-
-                    var list = JsonSerializer.Deserialize<List<Speciality>>(json, options);
-
-                    if (list != null) { Specialities.AddRange(list); }
-                }
 
 
             }
         }
+    }
 }
