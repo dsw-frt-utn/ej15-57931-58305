01cdca4f87f4c675b2d05875724bb68981ce6296
Lautaro <Lautaro.Herrera@alu.frt.utn.edu.ar>
2026-07-30 00:00:55 -0300

    CorrecionesDto

--- Dsw2026Ej15/Dsw2026Ej15.Api/DTOs/CreateDoctorDto.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Api/DTOs/CreateDoctorDto.cs
@@ -1,11 +1,8 @@
 ﻿namespace Dsw2026Ej15.Api.DTOs
 {
-    public class CreateDoctorDto
+    public record CreateDoctorDto
     {
-        public string _name { get; set; } = string.Empty;
-        public string _licenseNumber { get; set; } = string.Empty;
-        public Guid _specialityId { get; set; }
-        public bool _isActive { get; set; } = true;
-
+        public record Request(string Name, string LicenceNumber, Guid SpecialityId);
+        public record Response(Guid Id, string Name, string LicenceNumber, string SpecialtyName);
     }
 }
--- Dsw2026Ej15/Dsw2026Ej15.Data/DTOs/SpecialityDto.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Data/DTOs/SpecialityDto.cs
@@ -4,10 +4,5 @@ using System.Text;
 
 namespace Dsw2026Ej15.Data.DTOs
 {
-    internal class SpecialityDto
-    {
-        public Guid _id { get; set; }
-        public string _name { get; set; } = string.Empty;
-        public string _description { get; set; } = string.Empty;
-    }
+    public record SpecialityDto(Guid Id, string Name, string Description);
 }
